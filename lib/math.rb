95c220470822bc0a7d8e557d07bae3eb8c04d154
Matine temory <temorymatine@gmail.com>
2020-05-15 20:35:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,52 @@
-def addition(num1, num2)
 
+num1=5
+num2=10
+num=2
+
+def addition(num1, num2)
+  num2+num1=sum
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+ num2-num1=sum
+  puts sum
+    sum
 end
 
 def division(num1, num2)
-
+num1/num2=sum
+  puts sum
+    sum
 end
 
 def multiplication(num1, num2)
-
+ num1*num2=sum
+  puts sum
+    sum
 end
 
 def modulo(num1, num2)
-
+num1%num2=sum
+  puts sum
+    sum
 end
 
 def square_root(num)
-
+Math.sqrt(num)
+  puts num
+    num
 end
+
+def all 
+  addition
+  subtraction
+  division
+  multiplication
+  modulo
+  square_root
+end
+
+all
+  
