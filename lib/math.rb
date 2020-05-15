106a79c6a5bfeca5e8c7cb33d56bdfad2149b318
Matine temory <temorymatine@gmail.com>
2020-05-15 20:39:58 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
-def addition(num1, num2)
 
+def addition(num1, num2)
+  return num2+num1
 end
 
 def subtraction(num1, num2)
-
+ return num1-num2
+  
 end
 
 def division(num1, num2)
-
+return num1/num2
+  
 end
 
 def multiplication(num1, num2)
-
+ return num1*num2
+  
 end
 
 def modulo(num1, num2)
-
+return num1%num2
+ 
 end
 
 def square_root(num)
-
+return Math.sqrt(num)
+  
 end
+
